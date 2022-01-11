c9b9652debe3a74473fbf2ac291570aa0dca3a82
ccafer <61919465+cenkcafer@users.noreply.github.com>
2022-01-11 17:22:13 -0500

    new push jan 11 521pm mac

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from quote_generator import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', views.home, name='home'),
+    path('', views.index, name='index'),
+   # path('about/', views.about, name='about'),
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
